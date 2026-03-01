9049e95c96587528d7c70624d64f78c3a416b44e
braxv1 <joelsmith1094@gmail.com>
2026-03-01 19:04:18 +0400

    Update BAudio.lua

--- Libraries/BAudio.lua
+++ Libraries/BAudio.lua
@@ -4,18 +4,19 @@
 
 local library = {}
 
--- Args: url, keepold, filename, volume, speed, deleteonfinish
+-- args: url, keepold, filename, volume, speed, deleteonfinish
 function library.play(url, keepold, filename, volume, speed, deleteonfinish)
     
     -- check if we already have the file
     local fileexists = isfile(filename)
+    
     -- if we dont want to keep the old one or we dont have it yet we download
     if not keepold or not fileexists then
         -- wipe the old one if it exists so we can get a new version
         if fileexists then delfile(filename) end
         
         local success, data = pcall(function() 
-            return game:httpget(url) 
+            return game:HttpGet(url) 
         end)
         
         if success and data then
@@ -27,15 +28,18 @@ function library.play(url, keepold, filename, volume, speed, deleteonfinish)
     end
     
     -- standard shit
-    local sound = instance.new("sound", game:getservice("soundservice"))
-    sound.soundid = getcustomasset(filename)
-    sound.volume = volume or 1
-    sound.playbackspeed = speed or 1
-    sound:play()
+    local sound = Instance.new("Sound")
+    sound.Name = "BraxSound"
+    sound.SoundId = getcustomasset(filename)
+    sound.Volume = volume or 1
+    sound.PlaybackSpeed = speed or 1
+    sound.Parent = game:GetService("SoundService")
+    
+    sound:Play()
     
     -- removes the sound on finish
-    sound.ended:connect(function()
-        sound:destroy()
+    sound.Ended:Connect(function()
+        sound:Destroy()
         
         -- only deletes the file from the folder if deleteonfinish is true
         if deleteonfinish and isfile(filename) then
